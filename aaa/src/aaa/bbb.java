59957a11e4bf8c6414c7682b31a0741e9765f038
yiren15678841732 <50288843+yiren15678841732@users.noreply.github.com>
2019-05-08 21:03:50 +0800

    Update bbb.java

--- aaa/src/aaa/bbb.java
+++ aaa/src/aaa/bbb.java
@@ -4,7 +4,11 @@ public class bbb {
 	public static void main(String[] args) {
 
 		System.out.println("2226666");
+		//你的
 		
-
+		
+		System.out.println("1111");
+		System.out.println("1111000");
+               //人家的
 	}
 }
