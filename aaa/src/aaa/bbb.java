e65b91cbba0d24dde62e62d3ffa848a50bce8b89
yiren15678841732 <50288843+yiren15678841732@users.noreply.github.com>
2019-05-08 20:40:55 +0800

    Update bbb.java

--- aaa/src/aaa/bbb.java
+++ aaa/src/aaa/bbb.java
@@ -2,6 +2,6 @@ package aaa;
 
 public class bbb {
 	public static void main(String[] args) {
-		System.out.println("cc0199999999999999999999901c");
+		System.out.println("cc010000000000001c");
 	}
 }
