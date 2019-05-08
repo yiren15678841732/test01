757e0d6576b8dcab05c767a565ad9e246a118911
UID <UID@DESKTOP-DACJOCN>
2019-05-08 21:08:55 +0800

    012

--- aaa/src/aaa/bbb.java
+++ aaa/src/aaa/bbb.java
@@ -6,7 +6,13 @@ public class bbb {
 		System.out.println("0001111222");
 
 		System.out.println("2226666");
+		System.out.println("2226666");
+		//浣犵殑
+		
 		
 		System.out.println(00000);
+		System.out.println("1111");
+		System.out.println("1111000");
+               //浜哄鐨�
 	}
 }
