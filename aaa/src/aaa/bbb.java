8cf9f77222743ce48aecc7860fc86fd41a27edbb
yiren15678841732 <50288843+yiren15678841732@users.noreply.github.com>
2019-05-08 20:54:13 +0800

    Update bbb.java

--- aaa/src/aaa/bbb.java
+++ aaa/src/aaa/bbb.java
@@ -4,11 +4,7 @@ public class bbb {
 	public static void main(String[] args) {
 
 		System.out.println("cc0111111111111521579801c");
-		System.out.println("cc01111121579801c");
-
-		System.out.println("cc010000000000001c");
-                System.out.println("cc010000000000001c");
-		System.out.println("cc010000000000001c");
+		
 
 	}
 }
